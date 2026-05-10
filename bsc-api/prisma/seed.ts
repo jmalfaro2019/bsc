aaf3d15f0faa7aa6c02ddfae94035e40f55db0c2
Jose Alfaro <joalfaroc@unal.edu.co>
2026-05-10 22:33:40 +0200

    fix: seed.ts import from @prisma/client instead of generated path, remove debug files from tracking

--- bsc-api/prisma/seed.ts
+++ bsc-api/prisma/seed.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "../src/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
